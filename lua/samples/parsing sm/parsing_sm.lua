972c3554b69efe786c7d8a94c9caff5e99eb1112
Anton Bondarenko <anton.bondarenko@google.com>
2024-05-31 03:21:14 +0300

    v9

--- lua/samples/parsing sm/parsing_sm.lua
+++
@@ -1,5 +0,0 @@
-local citylist = {}
-for line in io.lines("citys.csv") do
-    local city, region, coalition, coordinate_x, coordinate_y = line:match("%s*(.-),%s*(.-),%s*(.-),%s*(.-),%s*(.-)")
-    citylist[#citylist + 1] = { city = city, region = region, coalition = coalition, coordinate_x = coordinate_x, coordinate_y = coordinate_y }
-end
--- test/test_thread_sizes.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <pthread.h>
-
-int main() {
-	printf("sizeof(pthread_mutex_t) = %ld\n", sizeof(pthread_mutex_t));
-	printf("sizeof(pthread_cond_t) = %ld\n", sizeof(pthread_cond_t));
-	return 0;
-}
